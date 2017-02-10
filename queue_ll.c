eada06cfb51b3570f921bcf37ad523230bab8815
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2017-02-10 14:46:03 +0530

    acquireNode() : Remove explicit  declaration in malloc()

--- queue_ll.c
+++ queue_ll.c
@@ -288,7 +288,7 @@ Status acquireNode(){
 	//If it is a valid type
 	else{
 		//Acquire a space for the node from the memory
-		createdNode = (Node *)malloc(sizeof(struct Node));
+		createdNode = (Node *)malloc(sizeof(Node));
 		//Check if the memory is full
 		if(createdNode==NULL)
 			return NO_MEMORY_AVAILABLE;
