a0745a6a8d523c3e0bce0679d22d6a3b3b5b6497
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2017-02-10 12:41:54 +0530

    Fully redocumented

--- queue_ll.c
+++ queue_ll.c
@@ -1,6 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+/*
+	These are the basic status codes used throughout the program,
+	to indicate the status of a particular operation.
+	
+	Values : 	1. WRONG_OPTION_CHOOSEN : This code is returned by a method when the user
+	--------	------------------------- does not comply to the valid choices with the 
+				  		  associated menu
+			2. NO_MEMORY_AVAILABLE : This code is returned by acquireNode(), when the
+			------------------------ node creation process fails because of insufficient
+				 		 system memory, i.e. malloc() returns NULL.
+			3. QUEUE_OVERFLOW : This code is returned when the user tries to extend the
+			------------------- queue beyond its predefined limit, if any.
+			4. QUEUE_UNDERFLOW : This code is returned when traversal or deletion is
+			-------------------- performed over an empty queue.
+			5. INTERNAL_ERROR : This code is returned to depict an undefined system
+			------------------- behaviour, which the program should not face in general
+			    		    condition.
+			6. INVALID_POSITION_SPECIFIED : This code is returned when the user does not
+			------------------------------- select any of the front and rear positions to
+							perform an opeartion. This is typically binded
+							with Position:UNDEFINED, and the choice of the
+							position is just converted into a suitable status.
+			7. OP_SUCCESS : This code declares the successful execution of a particular
+			---------------	operation.
+*/
 typedef enum Status {
 	WRONG_OPTION_CHOOSEN,
 	NO_MEMORY_AVAILABLE,
@@ -11,6 +35,21 @@ typedef enum Status {
 	OP_SUCCESS
 } Status;
 
+/*
+	These are the basic operations performed over the queue, declared here
+	to make more generalized methods, just by 'switch'ing the appropiate type
+	to print the appropiate messages.
+	
+	Values :	1. CREATION : This indicates the operation of the creation of node,
+	--------	------------- typically by acquireNode().
+			2. INSERTION : This indicates the operation of inserting a node into
+			-------------- the queue, by addNode().
+			3. DELETION : This indicates the operation of deletion of a node from
+			------------- the queue, if any, by deleteNode().
+			4. TRAVERSAL : This indicates the operation of traversal of the queue,
+			-------------- by traverse().
+*/
+
 typedef enum OP_Type{
 	CREATION,
 	INSERTION,
@@ -18,11 +57,34 @@ typedef enum OP_Type{
 	TRAVERSAL
 } OP_Type;
 
+/*
+	These are the positions to perform operations over the queue.
+	
+	Values : 	1. FRONT : The front position of the queue.
+	--------	----------
+			2. REAR : The rear position of the queue.
+			---------
+			3. UNDEFINED : If user chooses an invalid option in selection,
+			--------------	typically causing return of INVALID_POSITION_SPECIFIED.
+*/
+
 typedef enum Position{
 	FRONT,
 	REAR,
 	UNDEFINED
 } Position;
+
+/*
+	These are the types of data that can be stored inside the queue,
+	and each node can contain any one of them.
+	
+	Members :	1. fval : A floating point value
+	---------	---------
+			2. ival : An integer value
+			---------
+			3. cval : A character value
+			---------
+*/
 
 typedef union Data {
 	float fval;
@@ -30,16 +92,39 @@ typedef union Data {
 	char cval;
 } Data;
 
+/*
+	This is the basic structure of each node in the queue.
+	
+	Members : 	1. type : The type of data stored inside the queue.
+	---------	--------- 1 indicates an integer value, 2 indicates
+				  a real value, a 3 indicates a character value.
+			2. value : The actual data of the node
+			----------
+			3. nextnode : The pointer to the next node in the queue.
+			-------------
+*/
+
 typedef struct Node {
 	int type;
 	Data value;
 	struct Node *nextNode;
 } Node;
 
+/*
+	The fundamental pointers to the front and rear position of the queue.
+	Both can be NULL if the queue is empty, can point to the same node
+	if the queue contains only one node, or point to the different
+	nodes if the size of the queue is >1.
+*/
+
 Node *front=NULL, *rear=NULL;
 
 /*
-	Prints the error message for a particular error code.
+	Prints the status message for a particular status code. This is done
+	to remove all the interface clutter from the actual operation code.
+	
+	Arguments : 	1. status : The status to analyze
+	-----------	2. op : The operation which returned the status
 */
 void printstatus(Status status, OP_Type op){
 	printf("\n");
@@ -77,6 +162,11 @@ void printstatus(Status status, OP_Type op){
 
 /*
 	Gets the user specified position for performing insertion or deletion.
+	
+	Arguments : 	op : The type of operation for which the position is required
+	-----------
+	Return value :	Any one of possible position values, based on
+	--------------	the selection of the user
 */
 Position getpos(OP_Type op){
 	int choice;
@@ -93,17 +183,27 @@ Position getpos(OP_Type op){
 }
 
 /*
-	Traverse the queue till 'upto'. 
-	Returns the Node at 'upto' if found, or NULL.
+	Traverse and display the queue.
+	
+	Return value : Any one of the following statuses,
+	--------------	1. QUEUE_UNDERFLOW
+			2. INTERNAL_ERROR : If the type of a node is <1 or >3,
+					    which shouldn't be the case whatsoever.
+			3. OP_SUCCESS
 */
 Status traverse(){
 	int count = 1, type;
+	//Point to the front of the queue
 	Node *aNode = front;
+	//Check if any item is at all present in the queue
 	if(aNode==NULL)
 		return QUEUE_UNDERFLOW;
+	//Start traversal
 	while(aNode!=NULL){
 		printf("Node : %d\nValue : ",count++);
+		//Check the type of the node
 		type = aNode->type;
+		//Print the value associated with the type
 		switch(type){
 			case 1: printf("%d",aNode->value.ival);
 				break;
@@ -114,6 +214,7 @@ Status traverse(){
 			default: return INTERNAL_ERROR;
 		}
 		printf("\n");
+		//Switch to next node
 		aNode = aNode->nextNode;
 	}
 	return OP_SUCCESS;
@@ -130,26 +231,41 @@ int retry(){
 }
 /*
 	Method for acquiring a node from the user. This method saves the acquired node in the global
-	createdNode, for later use - typically by addNode().
+	variable createdNode, for later use - typically by addNode().
+	
+	Return value : This method returns one of the following statuses,
+	--------------	1. WRONG_OPTION_CHOOSEN
+			2. NO_MEMORY_AVAILABLE
+			3. OP_SUCCESS
 */
 Node *createdNode = NULL;
 Status acquireNode(){
 	int type;
+	//Reset the pointer
 	createdNode = NULL;
+	//Ask user for the type of the node
 	printf("Enter the type of Node you want to add : \n");
 	printf("1. Integer\n2. Real\n3. Character : ");
 	scanf("%d",&type);
+	//check if it is at all a valid type
 	if(type<1 || type>3)
 	{
+		//If not, ask the user to retry
 		if(retry())
-			return acquireNode(); 
+			return acquireNode();
+		//If the user denies, return the status
 		return WRONG_OPTION_CHOOSEN;
 	}
+	//If it is a valid type
 	else{
+		//Acquire a space for the node from the memory
 		createdNode = (Node *)malloc(sizeof(struct Node));
+		//Check if the memory is full
 		if(createdNode==NULL)
 			return NO_MEMORY_AVAILABLE;
+		//Set the type of the node based on previous user selection
 		createdNode->type = type;
+		//Take input of the value for that particular type by the user
 		if(type==1)
 		{
 			printf("Enter the integer value you want to store : ");
@@ -170,89 +286,158 @@ Status acquireNode(){
 
 /*
 	Insert a Node in the queue. Here pos signifies the position to insert, typically front or rear, if present.
-	The possible 'Status'es of this operation are :
-	1. INTERNAL_ERROR : When the value of aNode is NULL, which it shouldn't be
-	2. INVALID_POSITION_SPECIFIED
-	3. OP_SUCCESS
+	
+	Arguments :	1. pos : The position in which the new node is to be inserted
+	-----------	2. aNode : The node to be inserted
+	Return value : One of the following statuses,
+	--------------	1. INTERNAL_ERROR : When the value of aNode is NULL, which it shouldn't be
+			2. INVALID_POSITION_SPECIFIED
+			3. OP_SUCCESS
 */
 Status addNode(Position pos, Node *aNode){
 	Node *temp;
 	int type;
+	//Check if the node given to insert is NULL
 	if(aNode==NULL)
 		return INTERNAL_ERROR;
+	//Check if the position is valid
 	if(pos==UNDEFINED)
 		return INVALID_POSITION_SPECIFIED;
+	//Check if the insertion is to be performed at the front
 	if(pos==FRONT){
+		//Check if at all any node is present in the queue
 		if(front==NULL){
+			//If not, set front and rear to this node
 			front=aNode;
 			rear=aNode;
 		}
+		//If any node is present in the queue
 		else{
+			//Insert front at the end of this node
 			aNode->nextNode = front;
+			//Set front to the present node
 			front = aNode;
 		}
 	}
+	//Check if the insertion is to be performed at the rear
 	else if(pos==REAR){
+		//Check if at all any node is present in the queue
 		if(rear==NULL){
+			//If not, set front and rear to this node
 			front = aNode;
 			rear=aNode;
 		}
+		//If any node is present in the queue
 		else{
+			//Insert the node at the end to the queue
 			rear->nextNode = aNode;
 			aNode->nextNode = NULL;
+			//Set rear to this node
 			rear = aNode;
 		}
 	}
 	return OP_SUCCESS;
 }
 
+/*
+	Delete a node from the queue.
+	
+	Arguments :	1. pos : The position from which the node is to be deleted
+	-----------
+	Return value :	One of the following statuses,
+	--------------	1. INVALID_POSITION_SPECIFIED
+			2. QUEUE_UNDERFLOW
+			3. OP_SUCCESS
+*/
+
 Status deleteNode(Position pos){
-	Node *del, *temp;
+	Node *del;
+	//Check if the position is at all valid
 	if(pos==UNDEFINED)
 		return INVALID_POSITION_SPECIFIED;
+	//Check if there is any node present in the queue
 	if(front==NULL)
 		return QUEUE_UNDERFLOW;
+	//Point to the first node
 	del = front;
+	//Check if there is only one node in the queue
 	if(front==rear){
+		//Free the only node
 		free(front);
+		//Reset front and rear to NULL
 		front = NULL;
 		rear = NULL;
 	}
+	//Check if the deletion is to be performed on the front
 	else if(pos==FRONT){
+		//Set front to the second node
 		front = front->nextNode;
+		//Free front
 		free(del);
 	}
+	//Deletion is to performed at the rear end
 	else{
+		/* Get the penultimate node of the queue.
+		   We need this because queue can only propagate
+		   linearly, from front to rear. To delete the rearmost
+		   node, we need to set the nextNode pointer of
+		   (rear-1)th node to NULL, so that the link is actually
+		   broken.
+		   This is done by checking the 2nd next node of the present
+		   node. If the 2nd next node is NULL, next node is rear,
+		   and hence present node is the one containing the link to it.
+		*/
 		while(del->nextNode->nextNode!=NULL){
+			//Propagate to the next node
 			del = del->nextNode;
 		}
+		//Free the next node from memory
 		free(del->nextNode);
+		//Break the link
 		del->nextNode = NULL;
 	}
 	return OP_SUCCESS;
 }
 
+/*
+	Main wrapper to the program. User will see only this menu,
+	and all of the operations will return back to this method.
+*/
+
+
 int main(){
 	int choice = 1;
+	//Print welcome message
 	printf("Welcome to QueueSimulator\n");
 	printf("=========================\n");
+	//Add atleast one node before performing any operation
 	printf("Before we continue, add at least one Node.\n");
-	if(acquireNode()==OP_SUCCESS)
+	//Acquire a new node from the user and check if it succeeds.
+	if(acquireNode()==OP_SUCCESS){
+		//If it does, add the node to the queue, and print the status
 		printstatus(addNode(FRONT, createdNode), INSERTION);
+	}
 	else{
+		//If the first node can't be created, exit from the program immediately
 		printf("Fatal error! Program will now terminate!");
 		choice = 5;
 	}
+	//The infinite choice-loop
 	while(choice>0 && choice<4){
+		//Get user choice
 		printf("\nEnter 1 to insert a new Node\nEnter 2 to delete an existing Node\nEnter 3 to traverse existing Nodes\n");
 		printf("Press any other key to exit : ");
 		scanf("%d",&choice);
 		printf("\n");
 		switch(choice){
-			case 1: printstatus(acquireNode(), CREATION);
+			case 1: //Acquire a new node, and print the status
+				printstatus(acquireNode(), CREATION);
+				//Ask the position of insertion, insert it in the queue and print the status
 				printstatus(addNode(getpos(INSERTION), createdNode), INSERTION); break;
-			case 2: printstatus(deleteNode(getpos(DELETION)), DELETION); break;
-			case 3: printstatus(traverse(), TRAVERSAL); break;
+			case 2: //Ask point of deletion, delete the node, and print the status
+				printstatus(deleteNode(getpos(DELETION)), DELETION); break;
+			case 3: //Traverse the queue, and print the status
+				printstatus(traverse(), TRAVERSAL); break;
 		}
 	}
 }
