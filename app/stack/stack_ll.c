879581563a7ab0e97a754534fba82c758897fd13
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2017-04-12 23:22:05 +0530

    Stack app implementation

--- app/stack/stack_ll.c
+++ app/stack/stack_ll.c
@@ -1,16 +1,48 @@
 #include<stdio.h>
 #include<stack.h>
-#define CONFIG_NODE_INTEGER
+
 int main(){
 	Stack *stack;
 	Node *node;
-	Data value;
-	value.ival = 20;
+	int ch;
+	printf("Creating stack..");
 	printStatus(initStack(&stack), STRUCTURE_CREATION);
-	printStatus(initNode(&node, INTEGER, value), NODE_CREATION);
-	printStatus(push(node, stack), INSERTION);
-	node = NULL;
-	printStatus(pop(&node, stack), DELETION);
-	printNode(node, 0);
+	printf("Do you want to specify stack limit?\n1. Yes\n2. No : ");
+	scanf("%d",&ch);
+	switch(ch){
+		case 1: printf("\nEnter the limit of the stack : ");
+			scanf("%d",&ch);
+			stack->limit = ch;
+	}
+	ch = 1;
+	while(ch>0 && ch<4){
+		printf("\n1. Push\n2. Pop\n3. Traverse\n4. Exit : ");
+		scanf("%d", &ch);
+		switch(ch){
+			case 1: printf("\nPushing a node");
+				printf("\n==============");
+				if(printStatus(createNode(&node), NODE_CREATION)==OP_SUCCESS){
+					if(printStatus(push(node, stack), INSERTION)!=OP_SUCCESS)
+						free(node);
+				}
+				printf("\n===============");
+				break;
+			case 2: printf("\nPopping a node");
+				printf("\n==============");
+				if(printStatus(pop(&node, stack), DELETION)==OP_SUCCESS){
+					printf("\nPopped item =>");
+					printNode(node, 0);
+				}
+				printf("\n==============");
+				break;
+			case 3: printf("\nTraversing the stack");
+				printf("\n====================");
+				printStatus(display(stack), TRAVERSAL);
+				printf("\n====================");
+				break;
+			default: printf("\nExiting program..");
+				 break;
+		}
+	}
 	return 0;
 }
